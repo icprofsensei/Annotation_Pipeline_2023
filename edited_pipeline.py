c3cb79282fe9a61e3630785bcd92936bfb78872c
Dhylan Patel <dp521@ic.ac.uk>
2023-07-14 20:33:38 +0100

    14/7

--- edited_pipeline.py
+++ edited_pipeline.py
@@ -13,7 +13,7 @@ from alive_progress import alive_bar
 class Annotator:
         def __init__(self, dic_directory, input_directory, output_directory):
             #Initialise inputs
-            self.dic_directory = dic_directory          #dic.json
+            self.dic_directory = dic_directory          #newdic.json
             self.input_directory = input_directory      #Input bioc files
             self.output_directory = output_directory    #Output file directory where Annotated_ouput is created. 
 
@@ -27,7 +27,7 @@ class Annotator:
             for i in dict_data:
                 CleanNames.append(i['CleanName'])
             CleanNames =list(dict.fromkeys(CleanNames))
-            print(CleanNames)
+            #print(CleanNames)
             list1=[]
             start_time = datetime.datetime.now() #start time
 
@@ -49,6 +49,7 @@ class Annotator:
             
                                       
             for in_file in PMC_files: 
+                    count = 0
                     with open(self.input_directory+ "/" + in_file , encoding = 'utf-8') as m_file:
                         if os.path.isfile(self.input_directory+ "/" + in_file) == True:
                                 print("Input file found")
@@ -65,29 +66,96 @@ class Annotator:
                                         wordlist=textsection.split(" ")
                                         #Iterates over a list of words in the wordlist, taken from the text section.
                                         
-                                        for word in wordlist:
-                                                        if word in CleanNames:
-                                                            match = next((l for l in dict_data if l['CleanName'] == word), None)
-                                                            my_list.append(str(word) + " " + str(match['CleanName']) + " " + str(match['TaxRank']))
-                                                            m['annotations'].append({
-                                                                                        "text":textsection,
-                                                                                        "infons":{
-                                                                                            "name":word,
-                                                                                            "identifier": match['CleanName'] ,
-                                                                                            "taxonomic rank": match['TaxRank'] ,
-                                                                                            "annotator":"dhylan.patel21@imperial.ac.uk",
-                                                                                            "date": time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()) ,
-                                                                                        },
-                                                                                        "id": match['TaxID'],
-                                                                                        "locations":{
-                                                                                            "length": len(textsection),
-                                                                                            "parent taxonomic id": match['ParentTaxID'],
-                                                                                            
-                                                                                        }
-                                                                                    })
-                                                        else:
-                                                            continue
-                                                    
+                                        for index, word in enumerate(wordlist):
+                                            if len(word) >= 4:
+                                                #Checks if word is an exact match
+                                                if word in CleanNames: 
+                                                      match = next((l for l in dict_data if l['CleanName'] == word), None)
+                                                      my_list.append(str(word) + " " + str(match['CleanName']) + " " + str(match['TaxRank']))
+                                                      count += 1
+                                                      m['annotations'].append({
+                                                                                "text":word,
+                                                                                "infons":{
+                                                                                    "identifier": "TAXRANK:"+ match['TaxID'] ,
+                                                                                    "type": match['TaxRank'] ,
+                                                                                    "annotator":"dhylan.patel21@imperial.ac.uk",
+                                                                                    "date": time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()) ,
+                                                                                    "parent_taxonomic_id": match['ParentTaxID']
+                                                                                },
+                                                                                "id": count,
+                                                                                "locations":{
+                                                                                    "length": len(word),
+                                                                                    "offset": index +1 ,
+                                                                                    
+                                                                                }
+                                                                            })
+                                                else:   
+                                                        possible = []
+                                                        for cn in CleanNames:
+                                                            if word in cn:
+                                                                x = cn.find(word)
+                                                                y = len(word)
+                                                                if  cn[x-1] ==" " and (cn.endswith(word) or cn[x+y]==" "):
+                                                                    possible.append(cn)
+                                                     #Possible is a list of clean names which contain a word from the text.               
+                                                        if len(possible) >=1:
+                                                            print("Possible: ", possible)
+                                                            
+                                                            for p in possible:
+                                                                
+                                                                p = p.split(" ")
+                                                                pos = p.index(word)
+                                                                if pos == 0 and (wordlist[index +1 ] in p):
+                                                       #Checks if the word before is a match         
+                                                                    p= " ".join(p)
+                                                                    for d in dict_data:
+                                                                        if d['CleanName'] == p:
+                                                                            match = d
+                                                                            my_list.append(str(p) + " " + str(match['CleanName']) + " " + str(match['TaxRank']))
+                                                                            count +=1
+                                                                            m['annotations'].append({
+                                                                                "text":word,
+                                                                                "infons":{
+                                                                                    "identifier": "TAXRANK:"+ match['TaxID'] ,
+                                                                                    "type": match['TaxRank'] ,
+                                                                                    "annotator":"dhylan.patel21@imperial.ac.uk",
+                                                                                    "date": time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()) ,
+                                                                                    "parent_taxonomic_id": match['ParentTaxID']
+                                                                                },
+                                                                                "id": count,
+                                                                                "locations":{
+                                                                                    "length": len(word),
+                                                                                    "offset": index +1 ,
+                                                                                    
+                                                                                }
+                                                                            })
+                                                         #Checks if the word after is a match
+                                                                elif pos >=0 and (wordlist[index -1] in p):
+                                                                     p= " ".join(p)
+                                                                     for d in dict_data:
+                                                                        if d['CleanName'] == p:
+                                                                            match = d
+                                                                            my_list.append(str(p) + " " + str(match['CleanName']) + " " + str(match['TaxRank']))
+                                                                            count +=1
+                                                                            m['annotations'].append({
+                                                                                "text":word,
+                                                                                "infons":{
+                                                                                    "identifier": "TAXRANK:"+ match['TaxID'] ,
+                                                                                    "type": match['TaxRank'] ,
+                                                                                    "annotator":"dhylan.patel21@imperial.ac.uk",
+                                                                                    "date": time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()) ,
+                                                                                    "parent_taxonomic_id": match['ParentTaxID']
+                                                                                },
+                                                                                "id": count,
+                                                                                "locations":{
+                                                                                    "length": len(word),
+                                                                                    "offset": index +1 ,
+                                                                                    
+                                                                                }
+                                                                            })
+                                                            
+                                                                    
+                                                        
                                         bar()
                                                 
                                         
