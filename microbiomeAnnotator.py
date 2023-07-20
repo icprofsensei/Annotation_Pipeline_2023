1df91db794a6a2b33de8b963d6dc7fa8d284bace
Dhylan Patel <dp521@ic.ac.uk>
2023-07-20 13:29:35 +0100

    changes_20/7

--- microbiomeAnnotator.py
+++ microbiomeAnnotator.py
@@ -3,7 +3,6 @@ import pandas as pd
 import json
 import re
 import time
-from collections import Counter
 import os
 import datetime
 import os.path
@@ -11,36 +10,31 @@ from alive_progress import alive_bar
 
 
 class Annotator:
-        def __init__(self, dic_directory, input_directory, output_directory):
+        def __init__(self, dic_directory, input_directory, output_directory, count):
             #Initialise inputs
             self.dic_directory = dic_directory          #newdic.json
             self.input_directory = input_directory      #Input bioc files
             self.output_directory = output_directory    #Output file directory where Annotated_ouput is created. 
+            self.count = count
 
         def initialsteps(self):
             folder = '/Annotated_output_' + str(time.strftime('%Y-%m-%d_%H-%M-%S',time.localtime()))
             os.mkdir(self.output_directory + folder)
-            f1 = open(self.dic_directory, encoding = 'utf-8') 
+            opendic = open(self.dic_directory, encoding = 'utf-8') 
             if os.path.isfile(self.dic_directory) == True:
                  print("Dictionary loaded")
-            dict_data = json.load(f1)
+            dict_data = json.load(opendic)
             CleanNames = []
             for i in dict_data:
                 CleanNames.append(i['CleanName'])
             CleanNames =list(dict.fromkeys(CleanNames))
-            #print(CleanNames)
-            list1=[]
+            taxalist=[]
             start_time = datetime.datetime.now() #start time
-
             message = '' #message with start and stop time
-
             all_files = os.listdir(self.input_directory) 
 
-            PMC_files=[]  
-            documents,passages=[],[]
-            id=0
-
 #PMC_files contains all files in the input which begin with PMC. 
+            PMC_files=[]  
             for n in all_files:   
                 if n.startswith('PMC') and n.endswith('bioc.json'):  
                     PMC_files.append(n)
@@ -48,24 +42,29 @@ class Annotator:
 
 #For each PMC file, the data is loaded as a json and my_list is made. Text is under documents --> passages --> annotations --> text --> word     
             
-                                      
-            for in_file in PMC_files: 
-                    count = 0
+            for i in range(len(PMC_files)):
+                    in_file = PMC_files[i]
+                    print("Annotating file: ", i + 1, "of: ", len(PMC_files))                         
+                    count = str(self.count)
                     with open(self.input_directory+ "/" + in_file , encoding = 'utf-8') as m_file:
                         if os.path.isfile(self.input_directory+ "/" + in_file) == True:
                                 print("Input file found")
                         data = json.load(m_file)
-                        my_list=[]
+                        taxa_per_file=[]
+                        documents = []
                         documents=data['documents'] 
                         for j in documents:
+                                passages = []
                                 passages= j['passages'] 
                                 total = len(passages)
                                 with alive_bar(total) as bar: 
                                     for m in passages:
                                         m['annotations']=[]      
                                         textsection=m['text']
+                                        offsetoftext = m['offset']
                                         wordlist=textsection.split(" ")
                                     #Iterates over a list of words in the wordlist, taken from the text section.
+                                        sentenceoffset = 0
                                         for index, word in enumerate(wordlist):
                                             for i in word:
                                                 if i.isalnum() == True:
@@ -76,24 +75,27 @@ class Annotator:
                                                         wordlist[index] = word.rstrip(".,")
                                                 else: 
                                                     continue
-                                            #print(word)
+                                        for index, word in enumerate(wordlist):
                                             if len(word) >= 4:
-                                                #Checks if word is an exact match
                                                 if word in CleanNames: 
-                                                        #print("Exact_match")
+                                                        #Exact_match
                                                         match = next((l for l in dict_data if l['CleanName'] == word), None)
-                                                        count +=1
-                                                        if wordlist[index + 1].rstrip(".,") in ['[sp]', '[spp]', '[species]']:
-                                                            modifier = "species" 
-                                                            my_list.append(str(word) + " " + str(match['CleanName']) + " " + modifier)
-                                                            self.AddAnnotation(word, match, count, index, m, modifier)
-                                                        elif wordlist[index + 1].rstrip(".,") in ['[ge]', '[genus]']:
-                                                            modifier = "genus" 
-                                                            my_list.append(str(word) + " " + str(match['CleanName']) + " " + modifier)
-                                                            self.AddAnnotation(word, match, count, index, m, modifier)
+                                                        if match['TaxRank'] == "genus":
+                                                             possible_species = word + " " + str(wordlist[index + 1])
+                                                             print(possible_species)
+                                                             if possible_species in CleanNames:
+                                                                  print("found")
+                                                                  continue
+                                                             else:
+                                                                  self.AddAnnotation(match, self.count, index, m, " ", taxa_per_file, sentenceoffset, offsetoftext)
+                                                        elif wordlist[index + 1].rstrip(".,") in ['[sp]', '[spp]', '[species]', 'sp', 'spp']:
+                                                            modifier = "species"
+                                                            self.AddAnnotation(match, self.count, index, m, modifier, taxa_per_file, sentenceoffset, offsetoftext)
+                                                        elif wordlist[index + 1].rstrip(".,") in ['[gen]', '[genus]', 'genus', 'gen']:
+                                                            modifier = "genus"
+                                                            self.AddAnnotation(match, self.count, index, m, modifier, taxa_per_file, sentenceoffset, offsetoftext)
                                                         else:
-                                                             my_list.append(str(word) + " " + str(match['CleanName']) + " " + str(match['TaxRank']))
-                                                             self.AddAnnotation(word, match, count, index, m, " ")
+                                                             self.AddAnnotation(match, self.count, index, m, " ", taxa_per_file, sentenceoffset, offsetoftext)
                                         
                                                 else:   
                                                         possible = []
@@ -105,10 +107,6 @@ class Annotator:
                                                                     possible.append(cn)
                                                         #Possible is a list of clean names which contain a word from the text.  If there is a possible list, do the following:             
                                                         if len(possible) >=1:
-                                                            '''
-                                                            if len(possible)<=10:
-                                                                print("Possible: ", possible)
-                                                                '''
                                                             for p in possible:
                                                                 p= p.split(" ")
                                                                 longest = max(0, len(p))
@@ -125,46 +123,46 @@ class Annotator:
                                                                         for d in dict_data:
                                                                                 if d['CleanName'] == section:
                                                                                     match = d
-                                                                                    count +=1
-                                                                                    my_list.append(str(section) + " " + str(match['CleanName']) + " " + str(match['TaxRank']))
-                                                                                    self.AddAnnotation(word, match, count, index, m, " ")
+                                                                                    self.AddAnnotation(match, self.count, index, m, " ", taxa_per_file, sentenceoffset, offsetoftext)
                                                                         break
                                                                 else:
                                                                             continue
+                                            sentenceoffset += (len(word)+ 1)
                                         bar()
 
                                                           
 
-                                my_list = {*my_list}
-                                my_list = list(my_list)
-                                for j in my_list:
+                                taxa_per_file = {*taxa_per_file}
+                                taxa_per_file = list(taxa_per_file)
+                                for j in taxa_per_file:
                                       print(j)
-                                list1.append(my_list)
+                                taxalist.append(taxa_per_file)
                                 for i in data: 
                                     a_file = open(self.output_directory + folder + "/" +str(in_file), "w")
                                     json.dump(data, a_file, indent = 4)
                                     a_file.close()
              
             stop_time = datetime.datetime.now() #stop time
-            print(id)
             message = 'Start time is ' + str(start_time) + '\n' + 'Stop time is ' + str(stop_time)  
 
-            with open(self.output_directory + folder + "/" + 'runtime11.log', 'a') as time_file:   #start and stop time are written into a runtime.log file
+            with open(self.output_directory + folder + "/" + 'runtime.log', 'a') as time_file:   #start and stop time are written into a runtime.log file
                 
             
                 time_file.write(message)
 
-            with open(self.output_directory + folder + "/" + 'runtime12new.log', 'a') as time_file:   #start and stop time are written into a runtime.log file
+            with open(self.output_directory + folder + "/" + 'taxafound.log', 'a') as time_file:   #All taxa found are added to taxafound.log file.
                 
                 time_file.write("This file contains all the kingdoms and taxonomic ranks for each species found.")
-                time_file.write(str(list1))
+                time_file.write(str(taxalist))
 
-        def AddAnnotation(self, word, match, count, index, m, modifier):
+        def AddAnnotation(self, match, count, index, m, modifier, taxa_per_file, sentenceoffset, offsetoftext):
+            self.count = int(count) + 1
+            
             if modifier != " ": 
                     dictannot = {
                                         "text":match['CleanName'],
                                         "infons":{
-                                            "identifier": "TAXRANK:"+ match['TaxID'] ,
+                                            "identifier":  match['TaxID'] ,
                                             "type": modifier ,
                                             "annotator":"dhylan.patel21@imperial.ac.uk",
                                             "date": time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()) ,
@@ -172,11 +170,12 @@ class Annotator:
                                         },
                                         "id": count,
                                         "locations":{
-                                            "length": len(word),
-                                            "offset": index +1 ,
+                                            "length": len(match['CleanName']),
+                                            "offset": sentenceoffset + offsetoftext ,
                                             
                                         }
                                     }
+                    taxa_per_file.append(str(match['CleanName']) + " " + str(match['CleanName']) + " " + modifier)
             else:
                     dictannot = {
                                         "text":match['CleanName'],
@@ -189,11 +188,11 @@ class Annotator:
                                         },
                                         "id": count,
                                         "locations":{
-                                            "length": len(word),
-                                            "offset": index +1 ,
+                                            "length": len(match['CleanName']),
+                                            "offset": sentenceoffset + offsetoftext ,
                                             
                                         }
                                     }
+                    taxa_per_file.append(match['CleanName'] + " " + str(match['CleanName']) + " " + str(match['TaxRank']))
+    
             m['annotations'].append(dictannot)
-
-  
--- microbiomeAnnotatorCaller.py
+++ microbiomeAnnotatorCaller.py
@@ -1,12 +1,15 @@
 #Annotator caller
 import PySimpleGUI as sg
 from microbiomeAnnotator import Annotator as ann
-
+'''
 jsondic = sg.popup_get_file("Location of NCBI_Tax_dictionary.json:")
 sg.popup('You entered', jsondic)
 inputfiles = sg.popup_get_folder("Folder of input bioc files:")
 sg.popup("You entered", inputfiles)
 outputfiles = sg.popup_get_folder("Directory to contain output json files:")
 sg.popup("You entered", outputfiles)
-result = ann(jsondic, inputfiles, outputfiles)
+result = ann(jsondic, inputfiles, outputfiles, 0)
+result.initialsteps()
+'''
+result = ann('NCBI_tax_dictionary.json', 'testset', 'results', 0)
 result.initialsteps()
