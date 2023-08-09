612c26d9384ac32df650323f801bd5322c4e8110
Dhylan Patel <dp521@ic.ac.uk>
2023-08-09 20:33:54 +0100

    new_dict

--- microbiomeAnnotatorCaller.py
+++ microbiomeAnnotatorCaller.py
@@ -1,7 +1,7 @@
 #Annotator caller
 import PySimpleGUI as sg
 from microbiomeAnnotator import Annotator as ann
-
+'''
 jsondic = sg.popup_get_file("Location of NCBI_Tax_dictionary.json:")
 sg.popup('You entered', jsondic)
 inputfiles = sg.popup_get_folder("Folder of input bioc files:")
@@ -11,6 +11,5 @@ sg.popup("You entered", outputfiles)
 result = ann(jsondic, inputfiles, outputfiles, 0)
 result.initialsteps()
 '''
-result = ann('NCBI_tax_dictionary4.json', 'Testset2', 'results', 0)
+result = ann('NCBI_tax_dictionary8.json', 'Testset3', 'results', 0)
 result.initialsteps()
-'''
