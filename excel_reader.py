9ebb872abe8b98f899f5ef2a407f9b1f028c4a77
Dhylan Patel <dp521@ic.ac.uk>
2023-11-14 17:47:18 +0000

    asdf

--- excel_reader.py
+++ excel_reader.py
@@ -1,3 +1,23 @@
 import pandas as pd
 df = pd.read_excel('microbiomeCorpusToDomain.xlsx')
-print(df)
+
+df_airway = df[df['Airway'] == 1]
+df_faecal = df[df['Faecal'] == 1]
+df_skin = df[df['Skin'] == 1]
+df_urinary = df[df['Urinary'] == 1]
+df_vaginal = df[df['Vaginal'] == 1]
+
+airway_ids = df_airway['PMCID'].tolist()
+faecal_ids = df_faecal['PMCID'].tolist()
+skin_ids = df_skin['PMCID'].tolist()
+urinary_ids = df_urinary['PMCID'].tolist()
+vaginal_ids = df_vaginal['PMCID'].tolist()
+
+print(airway_ids)
+'''
+print(df_airway)
+print(df_faecal)
+print(df_skin)
+print(df_urinary)
+print(df_vaginal)
+'''
